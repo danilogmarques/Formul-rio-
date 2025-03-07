be46a3dc6885e1a5cfd1041a66199aa216ef9b03
danilogmarques <danilogustavomarques@gmail.com>
2025-03-07 17:01:03 -0300

    alterando de h1 para label

--- formulario/src/components/formularioDireito/formularioDireito.tsx
+++ formulario/src/components/formularioDireito/formularioDireito.tsx
@@ -2,6 +2,11 @@ export function FormularioDireito () {
     return (
         <div>
             <h4>SEXO</h4>
+            <select name="Gênero" id="gênero"></select>
+            <option value={"masculino"}>Masculino</option>
+            <option value={"feminino"}>Feminino</option>
+            <h4>DEPARTAMENTO</h4>   
+            <select name="Estado" id="estado"></select>
         </div>
     )
 }
